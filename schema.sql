b78934017c6e30c388ae729de3a7788fadb31e45
Paul Morimoto <psmorimoto@gmail.com>
2018-12-19 17:27:50 -0800

    Create schema for tour_of_listing component

---
+++ schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS tour_of_listing;
+
+CREATE DATABASE tour_of_listing;
+
+USE tour_of_listing;
+
+CREATE TABLE listingphotos (
+  id INT NOT NULL AUTO_INCREMENT,
+  listingId INT NOT NULL,
+  photourl varchar(100) NOT NULL,
+  category varchar(50) NOT NULL,
+  categorytext varchar(250),
+  PRIMARY KEY (id)
+);
+
+/*
+  mysql -u root -p <schema.sql
+*/
